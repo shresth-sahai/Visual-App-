ae506d9526b4231ea2a53173c2ba3e525e2c0944
shresth-sahai <shresth0407@gmail.com>
2025-03-08 14:58:31 +0530

    redis template added

--- src/main/java/com/example/Visual/Product.java
+++ src/main/java/com/example/Visual/Product.java
@@ -1,49 +1,17 @@
 package com.example.Visual;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Entity
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private Double price;
 
-    public Product(Long id, String name, Double price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-    // Getters and Setters
 }
--- src/main/java/com/example/Visual/ProductService.java
+++ src/main/java/com/example/Visual/ProductService.java
@@ -1,11 +1,24 @@
 package com.example.Visual;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void addProductToCache(String key,Product product){
+        redisTemplate.opsForValue().set(key,product);
+
+    }
+    public Product getProductFromCache(String key){
+        return (Product) redisTemplate.opsForValue().get(key);
+    }
+
     @Autowired
     public ProductService (ProductRepository productRepository){
         this.productRepository=productRepository;
